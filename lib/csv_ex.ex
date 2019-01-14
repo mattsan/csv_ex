4c4f52816b056e7e4e59af45e9392ad6e435140e
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-01-14 13:35:17 +0900

    add csv parser

--- lib/csv_ex.ex
+++ lib/csv_ex.ex
@@ -3,16 +3,40 @@ defmodule CsvEx do
   Documentation for CsvEx.
   """
 
-  @doc """
-  Hello world.
+  @csv_pattern ~r/(?:^|,)(?=[^"]|(")?)"?((?(1)[^"]*|[^,"]*))"?(?=,|$)/
 
-  ## Examples
+  def load(csv, opts \\ []) when is_list(opts) do
+    options = opts |> Map.new()
 
-      iex> CsvEx.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    csv
+    |> split()
+    |> apply_headers(options)
   end
+
+  defp split(csv) do
+    csv
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn row ->
+      Regex.scan(@csv_pattern, row)
+      |> Enum.map(&List.last/1)
+    end)
+  end
+
+  defp apply_headers(source, %{headers: headers}) when headers or headers == :atom do
+    [hs | body] = source
+
+    hs =
+      case headers do
+        :atom -> hs |> Enum.map(&String.to_existing_atom/1)
+        _ -> hs
+      end
+
+    body
+    |> Enum.map(fn row ->
+      Enum.zip(hs, row)
+      |> Map.new()
+    end)
+  end
+
+  defp apply_headers(source, _), do: source
 end
--- test/csv_ex_test.exs
+++ test/csv_ex_test.exs
@@ -2,7 +2,83 @@ defmodule CsvExTest do
   use ExUnit.Case
   doctest CsvEx
 
-  test "greets the world" do
-    assert CsvEx.hello() == :world
+  describe "without headers" do
+    setup do
+      [
+        expect: [
+          ["abc", "def", "ghi"],
+          ["123", "456", "789"],
+          ["987", "654", "321"]
+        ]
+      ]
+    end
+
+    test "simple csv", %{expect: expect} do
+      csv_string = """
+      abc,def,ghi
+      123,456,789
+      987,654,321
+      """
+
+      assert CsvEx.load(csv_string) == expect
+    end
+
+    test "quoted cells", %{expect: expect} do
+      csv_string = """
+      "abc","def","ghi"
+      "123","456","789"
+      "987","654","321"
+      """
+
+      assert CsvEx.load(csv_string) == expect
+    end
+
+    test "quoted cells with commas and spaces" do
+      csv_string = """
+      "abc","def","ghi"
+      "1,3","4 6","7,9"
+      "9 7","6,4","3 1"
+      """
+
+      expect = [
+        ["abc", "def", "ghi"],
+        ["1,3", "4 6", "7,9"],
+        ["9 7", "6,4", "3 1"]
+      ]
+
+      assert CsvEx.load(csv_string) == expect
+    end
+  end
+
+  describe "with headers" do
+    test "simple csv" do
+      csv_string = """
+      abc,def,ghi
+      123,456,789
+      987,654,321
+      """
+
+      expect = [
+        %{"abc" => "123", "def" => "456", "ghi" => "789"},
+        %{"abc" => "987", "def" => "654", "ghi" => "321"}
+      ]
+
+      assert CsvEx.load(csv_string, headers: true) == expect
+    end
+
+    test "headers as atom" do
+      csv_string = """
+      abc,def,ghi
+      123,456,789
+      987,654,321
+      """
+
+      expect = [
+        %{abc: "123", def: "456", ghi: "789"},
+        %{abc: "987", def: "654", ghi: "321"}
+      ]
+
+      assert CsvEx.load(csv_string, headers: :atom) == expect
+    end
   end
 end
