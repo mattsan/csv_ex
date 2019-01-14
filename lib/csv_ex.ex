ffd1bfc8272102a6e461eb5be56a1d8744794104
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-01-14 18:07:54 +0900

    add documents

--- lib/csv_ex.ex
+++ lib/csv_ex.ex
@@ -5,6 +5,27 @@ defmodule CsvEx do
 
   @csv_pattern ~r/(?:^|,)(?=[^"]|(")?)"?((?(1)[^"]*|[^,"]*))"?(?=,|$)/
 
+
+  @doc """
+  Load a CSV data from a CSV formatted string.
+
+  ## Options
+
+    - `:headers` - if `true` elements of the first line are used as CSV headers.
+      Each line are parse as Map data.
+      If `:atom` keys of Map data as atom. In this case, this function use [`String.to_existing_atom/1`](https://hexdocs.pm/elixir/String.html#to_existing_atom/1).
+
+  ## Examples
+
+      iex> CsvEx.load("abc,def,ghi\\n123,456,789\\n987,654,321\\n")
+      [["abc", "def", "ghi"], ["123", "456", "789"], ["987", "654", "321"]]
+
+      iex> CsvEx.load("abc,def,ghi\\n123,456,789\\n987,654,321\\n", headers: true)
+      [%{"abc" => "123", "def" => "456", "ghi" => "789"}, %{"abc" => "987", "def" => "654", "ghi" => "321"}]
+
+      iex> CsvEx.load("abc,def,ghi\\n123,456,789\\n987,654,321\\n", headers: :atom)
+      [%{abc: "123", def: "456", ghi: "789"}, %{abc: "987", def: "654", ghi: "321"}]
+  """
   def load(csv, opts \\ []) when is_list(opts) do
     options = opts |> Map.new()
 
