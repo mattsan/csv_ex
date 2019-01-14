970efb93eb7fa846d8b7f2fa6adfb44c06dc072d
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-01-14 12:45:11 +0900

    initial commit

---
+++ lib/csv_ex.ex
@@ -0,0 +1,18 @@
+defmodule CsvEx do
+  @moduledoc """
+  Documentation for CsvEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CsvEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/csv_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule CsvExTest do
+  use ExUnit.Case
+  doctest CsvEx
+
+  test "greets the world" do
+    assert CsvEx.hello() == :world
+  end
+end
